d76c60cc4e5b2a22b5776007eb152bc1c5755785
Tomasini Marcello <whitehatty@gmail.com>
2013-07-02 00:20:25 -0400

    Spoof MAC Address

---
+++ MAClone.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+case $2 in
+[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F])
+        sudo ifconfig '$1' ether '$2';;
+*) echo "$2 is not a MAC Address";;
+esac
