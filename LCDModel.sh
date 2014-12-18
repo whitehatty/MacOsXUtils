27a6386fd5d4f99c686347c0c20db46b86797d6c
Tomasini Marcello <whitehatty@gmail.com>
2014-12-18 10:47:22 +0100

    updated to reflect http://whitehatty.com/2012/04/28/mac-os-x-display-edid/

--- LCDModel.sh
+++ LCDModel.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ioreg -lw0 | grep IODisplayEDID | sed "/[^<]*</s///" | xxd -p -r | strings -6
+ioreg -lw0 -r -c "IODisplayConnect" -n "display0" -d 2 | grep IODisplayEDID | sed "/[^<]*</s///" | xxd -p -r | strings -6
